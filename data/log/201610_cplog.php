70cf77d540a022a6eb9ff5559fd8c789083d9d19
xiasf <811800545@qq.com>
2016-10-29 03:35:45 +0800

    add .gitignore file

--- data/log/201610_cplog.php
+++ data/log/201610_cplog.php
@@ -1,3 +1,5 @@
 <?PHP exit;?>	1477558760	admin	1	127.0.0.1	styles	GET={}; POST={};
 <?PHP exit;?>	1477558760	admin	1	127.0.0.1	styles	GET={frames=yes; }; POST={};
 <?PHP exit;?>	1477558761	admin	1	127.0.0.1	styles	GET={}; POST={};
+<?PHP exit;?>	1477682988	admin	1	127.0.0.1		GET={}; POST={};
+<?PHP exit;?>	1477682992	admin	1	127.0.0.1	index	GET={}; POST={};
